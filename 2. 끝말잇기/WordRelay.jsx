4110685c1a6a942b1f4bc9bf319a4f866c9462b0
bohyeon7 <qhgus4856@gmail.com>
2024-08-13 16:48:13 +0900

    webpack dev server, hot reload, uncontrolled input

--- 2. 끝말잇기/WordRelay.jsx
+++ 2. 끝말잇기/WordRelay.jsx
@@ -1,11 +1,73 @@
 const React = require('react');
-const { useState } = React;
+const { useState, useRef } = React;
 
 const WordRelay = () => {
-  const [text, setText] = React.useState('Hello, Webpack!');
+  const [word, setWord] = useState('보현');
+  const [word2, setWord2] = useState('나보');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const [result2, setResult2] = useState('');
+  const inputRef = useRef(null);
+  const inputRef2 = useRef(null);
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  }
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
+    } else {
+      setResult('땡');
+      setValue('');
+    }
+
+    inputRef.current.focus();
+  }
+
+  const onSubmitForm2 = (e) => {
+    e.preventDefault();
+    console.log(inputRef2.current.value);
+    
+    if (word2[word.length - 1] === e.target.children.word2.value[0]) {
+      setResult2('딩동댕');
+      setWord2(e.target.children.word2.value);
+      e.target.children.word2.value = '';
+    } else {
+      setResult2('땡');
+      e.target.children.word2.value = '';
+    }
+
+    inputRef2.current.focus();
+  }
 
   return <>
-    <h1>{text}</h1>
+    <div>
+      <h3>controlled input</h3>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input 
+          ref={inputRef} 
+          value={value} 
+          onChange={onChangeInput} 
+          type="text" />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </div>
+
+    <div>
+      <h3>uncontrolled input</h3>
+      <div>{word2}</div>
+      <form onSubmit={onSubmitForm2}>
+        <input id='word2' ref={inputRef2} />
+        <button>입력!</button>
+      </form>
+      <div>{result2}</div>
+    </div>
   </>
 }
 
--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   name: 'wordrelay-setting',
@@ -18,13 +19,24 @@ module.exports = {
       test: /\.jsx?/, // js, jsx 파일에 적용
       loader: 'babel-loader', // 적용할 rule 이름
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'] // preset 옵션 명시
+        presets: ['@babel/preset-env', '@babel/preset-react'], // preset 옵션 명시
+        plugins: [
+          'react-refresh/babel'
+        ]
       }
     }]
   },
+  plugins: [
+    new RefreshWebpackPlugin()
+  ],
   // #3 출력될 파일
   output: {
     path: path.join(__dirname, 'dist'), // 현재폴더(node 가 알아서 찾아줌) 안에 있는 dist
-    filename: 'app.js'
+    filename: 'app.js',
+  },
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname) }, // 정적파일의 경로 설정
+    hot: true,
   }
 }
