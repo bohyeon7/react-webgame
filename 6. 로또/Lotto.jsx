3cbcc45dd214e6fe92ebe5183db6fcfb1d5e95af
bohyeon7 <qhgus4856@gmail.com>
2024-08-29 18:55:48 +0900

    useCallback

--- 6. 로또/Lotto.jsx
+++ 6. 로또/Lotto.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo, useCallback } from "react";
 import Ball from "./Ball";
 
 function getWinNumbers() {
@@ -15,7 +14,9 @@ function getWinNumbers() {
 }
 
 const Lotto = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers); // 당첨 숫자들 7개의 배열
+  const lottoNumbers = useMemo(() => getWinNumbers(), [redo]); // 함수 실행후 리턴값을 기억해둠
+  
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers); // 당첨 숫자들 7개의 배열
   const [winBalls, setWinBalls] = useState([]); // 앞에 6개의 공
   const [bonus, setBonus] = useState(null); // 보너스 공
   const [redo, setRedo] = useState(false); // 마지막 1개의 공 (보너스공까지 다 출력된 후 redo 버튼 보이게하기 위함)
@@ -24,17 +25,26 @@ const Lotto = () => {
   useEffect(() => {
     console.log('useEffect');
     
+    /**
+     * 0-5 까지 timeout 으로 winBalls 배열 생성
+     */
     for (let i = 0; i < winNumbers.length - 1; i++) {
       timeouts.current[i] = setTimeout(() => { // 1초마다 당첨공 배열에 저장
         setWinBalls((prevWinBalls) => [...prevWinBalls, winNumbers[i]]);
       }, 1000 * (i + 1));
     }
 
+    /**
+     * 6 번쨰 timeout 으로 winBalls 배열 추가
+     */
     timeouts.current[6] = setTimeout(() => { // 7초에 보너스공 저장
       setBonus(winNumbers[6]);
       setRedo(true);
     }, 7000);
 
+    /**
+     * timeout 7개 다 정리
+     */
     return () => {
       timeouts.current.forEach((v) => {
         clearTimeout(v);
@@ -44,20 +54,19 @@ const Lotto = () => {
 
   useEffect(() => {
     console.log('로또 숫자를 생성합니다');
-    console.log(winNumbers);
     
   }, [winNumbers]);
 
-  const onClickRedo = () => {
+  const onClickRedo = useCallback(() => {
     console.log('onClickRedo');
     
     // 초기화
-    setWinNumbers(getWinNumbers);
+    setWinNumbers(getWinNumbers());
     setWinBalls([]);
     setBonus(null);
     setRedo(false);
     timeouts.current = [];
-  }
+  }, [winNumbers]);
 
   return <>
     <div>당첨 숫자</div>
@@ -65,7 +74,6 @@ const Lotto = () => {
       {winBalls.map((v) => <Ball key={v} number={v} />)}
     </div>
     <div>보너스!</div>
-    {/* Ball 에는 onClick 함수가 왜있는거지??? */}
     {bonus && <Ball number={bonus} />}
     {redo && <button onClick={onClickRedo}>한 번 더!</button>}
   </>
