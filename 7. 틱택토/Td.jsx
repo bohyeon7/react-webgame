7e2e4c81e6ebc983dd486f8268363a54cf8da60e
bohyeon7 <qhgus4856@gmail.com>
2024-09-01 23:07:22 +0900

    최적화 (memo, useMemo)

--- 7. 틱택토/Td.jsx
+++ 7. 틱택토/Td.jsx
@@ -1,17 +1,17 @@
-import React, { useCallback } from "react";
+import React, { memo, useCallback } from "react";
 import { CLICK_CELL } from "./TicTacToe";
 
-const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
-  const onClickId = () => {
+const Td = memo(({ rowIndex, cellIndex, cellData, dispatch }) => {
+  const onClickId = useCallback(() => {
     console.log(rowIndex, cellIndex);
 
     if (cellData) return; // 이미 클릭했던 cell 은 변경안됨
 
     dispatch({ type: CLICK_CELL,  row: rowIndex, cell: cellIndex});
-  };
+  }, [cellData]);
   return <>
     <td onClick={onClickId}>{cellData}</td>
   </>
-}
+});
 
 export default Td;
--- 7. 틱택토/Tr.jsx
+++ 7. 틱택토/Tr.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { memo, useMemo } from "react";
 import Td from "./Td";
 
-const Tr = ({ rowIndex, rowData, dispatch }) => {
+const Tr = memo(({ rowIndex, rowData, dispatch }) => {
   return <>
   <tr>
-    {Array(rowData.length).fill().map((td, i) => (<Td key={i} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} dispatch={dispatch} />))}
+    {Array(rowData.length).fill().map((td, i) => (
+      useMemo(() => <Td key={i} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} dispatch={dispatch} />, [rowData[i]])
+    ))}
   </tr>
   </>
-}
+});
 
 export default Tr;
